3b1e0b68d2251d858872e7f7b209387cbda6a641
Joel <joel-digitals@web.de>
2026-06-14 11:52:22 +0200

    1.0.0.0_3

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-set -e
-
-echo "Starting Gunicorn..."
-gunicorn web_overlay.wsgi:application --bind 0.0.0.0:$PORT --chdir web --daemon
-
-echo "Starting Discord Bot..."
-exec node src/index.js
+echo "==> Starting Gunicorn (Django)..."
+cd web && gunicorn web_overlay.wsgi:application --bind 0.0.0.0:$PORT --workers 2 &
+echo "==> Starting Discord Bot..."
+cd /opt/render/project/src && exec node src/index.js
